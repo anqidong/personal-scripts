5d6e2561290ce61bccc5466e385014c0cb72600e
Anqi Dong <anqidong@users.noreply.github.com>
2023-08-17 13:21:49 -0700

    abbrs: Add more cases for non-full-word grepping

--- fish_conf/abbrs.fish
+++ fish_conf/abbrs.fish
@@ -1,5 +1,9 @@
-abbr -a ggrep git grep -Finw -C 5
-abbr -a --position anywhere grepw grep -Finw -C 5
+abbr -a ggrei git grep -Fin -C 5
+abbr -a ggrep git grep -Fin -C 5
+abbr -a --position anywhere grei grep -Fin -C 5
+
+abbr -a ggrew git grep -Finw -C 5
+abbr -a --position anywhere grew grep -Finw -C 5
 
 # For macOS `log` tool
 abbr -a slf set log_file
