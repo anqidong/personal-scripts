b06ea908d37edfc9d89888563456972f902b2106
Anqi Dong <anqidong@users.noreply.github.com>
2023-08-21 10:45:01 -0600

    config.fish: Add commands to pull in work-siloed stuff
    Work scripts folder will consist of bare paths rather than a bootstrap
    script, in order to reduce the amount of copypasta stored in the
    work-specific folder.

--- fish_conf/config.fish
+++ fish_conf/config.fish
@@ -15,16 +15,32 @@ if test -d "$HOME/git/personal-scripts/bin"
 end
 
 begin
-  set -l _test_dir (path dirname (status -f))"/functions"
-  if test -d $_test_dir
-    set -g -p fish_function_path $_test_dir
+  set -l _fish_func_dir (path dirname (status -f))"/functions"
+  if test -d $_fish_func_dir
+    set -g -p fish_function_path $_fish_func_dir
+  end
+
+  set -l _fish_abbrs_file (path dirname (status -f))"/abbrs.fish"
+  if test -f $_fish_abbrs_file
+    source $_fish_abbrs_file
   end
 end
 
+if test -d "$HOME/git/work-scripts/bin"
+  fish_add_path -g --path "$HOME/git/work-scripts/bin"
+end
+
 begin
-  set -l _test_file (path dirname (status -f))"/abbrs.fish"
-  if test -f $_test_file
-    source $_test_file
+  set -l _ws $HOME"/git/work-scripts/fish"
+
+  set -l _fish_func_dir $_ws"/functions"
+  if test -d $_fish_func_dir
+    set -g -p fish_function_path $_fish_func_dir
+  end
+
+  set -l _fish_abbrs_file $_ws"/abbrs.fish"
+  if test -f $_fish_abbrs_file
+    source $_fish_abbrs_file
   end
 end
 
