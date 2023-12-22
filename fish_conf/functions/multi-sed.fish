8b824c650b7a2924cb7923d930a356878dfcafe7
Anqi Dong <anqidong@users.noreply.github.com>
2023-12-22 00:37:11 -0600

    multi-sed: Convert to a fish function

---
+++ fish_conf/functions/multi-sed.fish
@@ -0,0 +1,22 @@
+function multi-sed
+  if test (count $argv) -lt 2
+    echo "usage: $_ <old_patt> <new_patt> [<files>]"
+    return -1
+  else if test (count $argv) -eq 2
+    set -l file_paths "."
+  else
+    set -l file_paths $argv[3..-1]
+  end
+
+  set -l from_str (string replace -a '/' '\/' $argv[1])
+  set -l to_str (string replace -a '/' '\/' $argv[2])
+
+  echo $from_str → $to_str
+
+  # for i in (grep -rl -Pz $argv[1] $file_paths);
+  for i in (grep -rl -E $argv[1] $file_paths);
+    # vim -c "%s/"$argv[1]"/"$argv[2]"/gc" -c "wq" $i
+    sed -i -E "s/"$from_str"/"$to_str"/g" $i
+    # perl -0777 -i -p -e "s/"$from_str"/"$to_str"/smg" $i
+  end
+end
