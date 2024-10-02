42b3d6add0bcbf232a90973c2a68a4f355233864
Anqi Dong <anqidong@users.noreply.github.com>
2024-10-02 15:36:03 -0700

    Add Python-based version of multi-sed
    This version is a bit more powerful and has fewer gotchas.
    
    Repurpose the fish function one to use the vim version. The Perl version
    doesn't work correctly with macOS (BSD) grep.

--- fish_conf/functions/multi-sed-vim.fish
+++ fish_conf/functions/multi-sed-vim.fish
@@ -1,4 +1,4 @@
-function multi-sed
+function multi-sed-vim
   if test (count $argv) -lt 2
     echo "usage: $_ <old_patt> <new_patt> [<files>]"
     return -1
@@ -17,8 +17,8 @@ function multi-sed
 
   # for i in (grep -rl -Pz $argv[1] $file_paths);
   for i in (grep -rl -E $argv[1] $file_paths);
-    # vim -c "%s/"$argv[1]"/"$argv[2]"/gc" -c "wq" $i
-    sed -E "s/"$from_str"/"$to_str"/g" $i > $i.tmp; mv $i.tmp $i
+    vim -c "%s/"$argv[1]"/"$argv[2]"/gc" -c "wq" $i
+    # sed -E "s/"$from_str"/"$to_str"/g" $i > $i.tmp; mv $i.tmp $i
     # perl -0777 -i -p -e "s/"$from_str"/"$to_str"/smg" $i
   end
 end
