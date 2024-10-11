050c51ae64022142be9aa4f03d066382126e8ddc
Anqi Dong <anqidong@users.noreply.github.com>
2024-10-11 15:34:42 -0700

    Add command to automatically exec into a running Docker container

---
+++ fish_conf/functions/docker-auto-exec.fish
@@ -0,0 +1,20 @@
+function docker-auto-exec
+  if test (count $argv) -eq 0
+    echo "Can't exec into docker with no args"
+    return -5
+  end
+
+  set -l _containers (docker ps --format "{{.Names}}")
+  if test (count $_containers) -ne 1
+    if test (count $_containers) -eq 0
+      echo "No containers"
+      return 5
+    else
+      echo "(FIXME) Not sure which container to pick amongst these:"
+      echo $_containers
+      return -3
+    end
+  end
+
+  docker exec -it $_containers $argv
+end
