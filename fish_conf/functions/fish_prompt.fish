ff092629e1210db4e96cc8df65aac34072851668
Anqi Dong <anqidong@users.noreply.github.com>
2024-10-11 00:04:33 -0700

    fish_prompt: A few more bells and whistles

--- fish_conf/functions/fish_prompt.fish
+++ fish_conf/functions/fish_prompt.fish
@@ -1,8 +1,21 @@
 # This overrides a builtin
 function fish_prompt -d "Write out the prompt"
-  printf '%s%s∘ %s%s@%s' (set_color 777) (date +"%d %a %H:%M") (set_color $fish_color_host) (whoami) (hostname|cut -d . -f 1)
+  set -l color_time 777
+  set -l suffix '>'
+  if functions -q fish_is_root_user; and fish_is_root_user
+      set color_time B83
+      set suffix '#'
+  end
+
+  set -l fancy_month \
+    (string unescape "\u"(string sub -s 3 (math -b 16 0x245F + (date +"%-m"))))
+
+  echo -ns (set_color $color_time) $fancy_month' ' (date +"%d %a %H:%M") '∘ ' \
+    (set_color $fish_color_host) (whoami) '@' (hostname|cut -d . -f 1)
+
   if test -n WINDOW -a "$WINDOW" != ''
     printf '%s(%s)' (set_color white) (echo $WINDOW)
   end
-  printf '%s> ' (set_color normal)
+
+  echo -ns (set_color normal) $suffix ' '
 end
