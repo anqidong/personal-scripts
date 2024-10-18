d77cc405fa11238883b8c0fb2854e58a1ca068b2
Anqi Dong <anqidong@users.noreply.github.com>
2024-10-18 13:50:21 -0700

    Don't attempt to source ROS stuff if ROS isn't installed

--- fish_conf/functions/_maybe_new_pwd.fish
+++ fish_conf/functions/_maybe_new_pwd.fish
@@ -1,3 +1,42 @@
+function _attempt_source_ros
+  if not functions -q bass
+    return
+  end
+
+  set -l maybe_path (ancestor-having-path "install/local_setup.bash")
+  if test -z "$maybe_path"
+    return
+  end
+
+  set -l ros_setup /opt/ros/humble/setup.bash
+  if not test -e "$ros_setup"
+    # ROS not installed on this machine, skip everything
+    return
+  end
+
+  # If this shell has never sourced any ROS stuff, try to also source the
+  # shared utils
+  if not set -q "$_auto_sourced_ros_local_setup"
+    echo "Sourcing $ros_setup"
+    bass source $ros_setup
+
+    set -l tb_setup ~/tb_ws/install/local_setup.bash
+    if test -e $tb_setup
+      echo "Sourcing $tb_setup"
+      bass source $tb_setup
+    end
+  end
+
+  if test "$maybe_path" != "~/tb_ws" -a \
+      "$maybe_path" != "$_auto_sourced_ros_local_setup"
+    set -g _auto_sourced_ros_local_setup $maybe_path
+    set -l local_setup $maybe_path"/install/local_setup.bash"
+    echo "Sourcing $local_setup"
+    bass source $local_setup
+  end
+
+end
+
 function _maybe_new_pwd -d \
     "Runs interactively, when the current directory may have changed"
 
@@ -13,30 +52,5 @@ function _maybe_new_pwd -d \
   end
 
   # Check for ROS stuff
-  set -l maybe_path (ancestor-having-path "install/local_setup.bash")
-  if test -n "$maybe_path" && functions -q bass
-    # If this shell has never sourced any ROS stuff, try to also source the
-    # shared utils
-    if not set -q "$_auto_sourced_ros_local_setup"
-      set -l ros_setup /opt/ros/humble/setup.bash
-      if test -e $ros_setup
-        echo "Sourcing $ros_setup"
-        bass source $ros_setup
-      end
-
-      set -l tb_setup ~/tb_ws/install/local_setup.bash
-      if test -e $tb_setup
-        echo "Sourcing $tb_setup"
-        bass source $tb_setup
-      end
-    end
-
-    if test "$maybe_path" != "~/tb_ws" -a \
-        "$maybe_path" != "$_auto_sourced_ros_local_setup"
-      set -g _auto_sourced_ros_local_setup $maybe_path
-      set -l local_setup $maybe_path"/install/local_setup.bash"
-      echo "Sourcing $local_setup"
-      bass source $local_setup
-    end
-  end
+  _attempt_source_ros
 end
