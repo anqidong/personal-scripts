e7884c57786dd967f16e0fe5d0210fcdf42d3096
Anqi Dong <anqidong@users.noreply.github.com>
2024-10-21 14:06:13 -0700

    _prettified_path: Use built-in to speed up

--- fish_conf/functions/_prettified_path.fish
+++ fish_conf/functions/_prettified_path.fish
@@ -1,4 +1,5 @@
 # This is a variant on the builtin prompt_pwd function
 function _prettified_path -d "Gets a prettified version of the current `pwd`"
-  echo -n (pwd | sed -e "s|^$HOME|~|")
+  # echo -n (pwd | sed -e "s|^$HOME|~|")
+  prompt_pwd -d 3 -D 5
 end
